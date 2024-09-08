f818c03ea820aa1b5a4c3b1418c8dfeb6cdf93b0
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-09-09 00:13:45 +0530

    done the styling and printing

--- Result-Calculation/result.java
+++ Result-Calculation/result.java
@@ -75,6 +75,7 @@ class Subject {
     public void displaySubject() {
        
         double roundedPercentage = roundToThreeDecimal(percentage);
+        System.out.println("-".repeat(100));
         System.out.println(subname + "               " + totalMarks + "                " + marksObtained + "                " + grade + "              " + roundedPercentage + "               " + pointer);        
     }
 }
@@ -111,78 +112,96 @@ class Table {
         };
     }
 
-    public void createTable(Scanner sc) {
-        String name;
-        System.out.print("Enter the name of the student: ");
-        name = sc.nextLine();
+   public void createTable(Scanner sc) {
+    String name;
+    System.out.print("Enter the name of the student: ");
+    name = sc.nextLine();
 
-        int internalKTs = 0;
-        int externalKTs = 0;
-        int termWorkKTs = 0;
-        int totalMarksOfStudent = 0;
-        int totalCredits = 0;
-        int obtainedCredited = 0; // To store the sum of (pointer * credit)
-        int totalMarksOutoff = 0;
+    int internalKTs = 0;
+    int externalKTs = 0;
+    int termWorkKTs = 0;
+    int totalMarksOfStudent = 0;
+    int totalCredits = 0;
+    int obtainedCredited = 0; // To store the sum of (pointer * credit)
+    int totalMarksOutoff = 0;
+    boolean hasFailed = false;
+    int totalKTs; // Flag to track if the student has failed any subject
 
-        for (int i = 0; i < subjects.length; i++) {
-            System.out.print("Enter marks for " + subjects[i].subname + ": ");
-            subjects[i].marksObtained = sc.nextInt();
-            subjects[i].calculateResult();
+    for (int i = 0; i < subjects.length; i++) {
+        System.out.print("Enter marks for " + subjects[i].subname + ": ");
+        subjects[i].marksObtained = sc.nextInt();
+        subjects[i].calculateResult();
 
-            
-            if (subjects[i].totalMarks <= 20 && subjects[i].result.equals("F")) {
-                internalKTs++;
-            }
-           
-            if (subjects[i].totalMarks > 20 && subjects[i].totalMarks != 25 && subjects[i].result.equals("F")) {
-                externalKTs++;
-            }
-           
-            if (subjects[i].totalMarks == 25 && subjects[i].result.equals("F")) {
-                termWorkKTs++;
-            }
-
-            if (subjects[i].credit > 0) { 
-                obtainedCredited += subjects[i].pointer * subjects[i].credit;
-                totalCredits += subjects[i].credit;
-            }
-
-            totalMarksOfStudent += subjects[i].marksObtained;
-            totalMarksOutoff   += subjects[i].totalMarks;
+        if (subjects[i].result.equals("F")) {
+            hasFailed = true; // Set flag if the student has failed any subject
         }
 
-        int totalKTs = internalKTs + externalKTs;
-           
-       
-        System.out.println("\nSubject             Total Marks        Marks Obtained        Pass/Fail        Percentage        Pointer");
-        System.out.println("-".repeat(100));
-        for (int i = 0; i < subjects.length; i++) {
-            subjects[i].displaySubject();
+        if (subjects[i].totalMarks <= 20 && subjects[i].result.equals("F")) {
+            internalKTs++;
         }
 
-        System.out.println("-".repeat(100));
+        if (subjects[i].totalMarks > 20 && subjects[i].totalMarks != 25 && subjects[i].result.equals("F")) {
+            externalKTs++;
+        }
 
+        if (subjects[i].totalMarks == 25 && subjects[i].result.equals("F")) {
+            termWorkKTs++;
+        }
+
+        if (subjects[i].credit > 0) { 
+            obtainedCredited += subjects[i].pointer * subjects[i].credit;
+            totalCredits += subjects[i].credit;
+        }
+
+        totalMarksOfStudent += subjects[i].marksObtained;
+        totalMarksOutoff   += subjects[i].totalMarks;
+    }
+
+     totalKTs = internalKTs + externalKTs;
+
+    System.out.println("-".repeat(100));
+    System.out.println("\nSubject             Total Marks        Marks Obtained        Pass/Fail        Percentage        Pointer");
+    for (int i = 0; i < subjects.length; i++) {
+        subjects[i].displaySubject();
+    }
+
+    System.out.println("-".repeat(100));
+         
+    System.out.println("     ");
+    System.out.println("\nResults for        " + name);
+    System.out.println("     ");
+    System.out.println("Total Internal KTs:  " + internalKTs);
+    System.out.println("     ");
+    System.out.println("Total External KTs:  " + externalKTs);
+    System.out.println("     ");
+    System.out.println("Total Term Work KTs: " + termWorkKTs);
+    System.out.println("     ");
+    System.out.println("Total Marks obtained:" + totalMarksOfStudent);
+    System.out.println("     ");
+    System.out.println("Total Marks :        "+totalMarksOutoff);
+    System.out.println("     ");
+    System.out.println("TotalKts             "+totalKTs);
+    System.out.println("     ");
+   
+    if (totalKTs > 9) {
+        System.out.println("Year drop");
+    }
+
+    if (hasFailed) {
+         
+        System.out.println("Unsuccessful");
+        System.out.println("     ");
+    } else {
         
-        System.out.println("\nResults for " + name);
-        System.out.println("Total Internal KTs: " + internalKTs);
-        System.out.println("Total External KTs: " + externalKTs);
-        System.out.println("Total Term Work KTs: " + termWorkKTs);
-        System.out.println("Total Marks obtained: " + totalMarksOfStudent);
-        System.out.println("Totalmarks : "+totalMarksOutoff);
-
-        
-        if (totalKTs > 9) {
-            System.out.println("Year drop");
-        }
-
-        // Calculate and display the CGPA
         double cgpa = (double) obtainedCredited / totalCredits;
         double roundedCGPA = roundToThreeDecimal(cgpa);
         System.out.println("CGPI : " + roundedCGPA);
-        System.out.println("-".repeat(100));
+    }
+    System.out.println("-".repeat(100));
+}
 
       
-    }
+    
 
     private double roundToThreeDecimal(double value) {
         return Math.round(value * 100.0) / 100.0;
